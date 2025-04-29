4f36560c063951071c1a2acb184b89beedfe5304
aachan1127 <aachan1127y@gmail.com>
2025-04-29 21:15:27 +0900

    laravel復習

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// アロー関数で書くとこうなる
+Route::get('/hello-world', fn() => view('hello_world'));
+
+// 連想配列
+Route::get('/hello', fn() => view('hello', [
+    'name' => '山田',
+    'course' => 'Laravel'
+]));
+
+// 最初に表示される画面
+Route::get('/', fn() => view('index'));
+
+Route::get('/curriculum', fn() => view('curriculum'));
